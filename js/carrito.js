ab86354c641d67cb46320f27298ade2a49a55716
martinrioss <martinrios.rrr@gail.com>
2022-12-23 15:59:18 -0300

    carrito listo

--- js/carrito.js
+++ js/carrito.js
@@ -1,5 +1,6 @@
 let contenedor=document.getElementById('carrito')
 let carrito=JSON.parse(localStorage.getItem('carrito')) || []
+let btnClear=document.querySelector('.clear-carrito')
 function renderCards (datos,contenedor){
     contenedor.innerHTML=''
     let contenedorCarrito=''
@@ -7,7 +8,8 @@ function renderCards (datos,contenedor){
         contenedorCarrito='<h1 class="h1-carrito">No hay productos en el carrito</h1>'
     }
     datos.forEach(element => { 
-      
+        let inputId = `input-${element._id}`
+        let input=`input-number${element._id}`
         let esta=carrito.some(e=>e._id===element._id)
         contenedorCarrito += 
         `<div class="card-carrito card mb-3 col-7">
@@ -19,8 +21,8 @@ function renderCards (datos,contenedor){
           <div class="col-md-8">
             <div class="card-body">
               <h5 class="card-title">${element.producto}</h5>
-              <p class="card-text">$${element.precio}</p>
-              <input class='input-class' type="number" value="1" min='1' max='${element.disponibles}'>
+              <p id="${inputId}"class="card-text">${element.precio}</p>
+              <input onclick="inputCant('${element._id}')" class='input-class ${input} ' type="number" value="" min='1' max='${element.disponibles}'>
               <p>${element.disponibles<5?`<span>Ultimos ${element.disponibles} unidades!</span>`:`Disponibles ${element.disponibles}`}</p>
               <button type="button" class="boton btn btn-secondary" id='${element._id}' onclick="handleclick('${element._id}')" ><img src="../img/close.png" alt="">Eliminar</button>
             </div>
@@ -32,10 +34,19 @@ function renderCards (datos,contenedor){
     contenedor.innerHTML=contenedorCarrito
 }
 renderCards(carrito,contenedor)
+function inputCant(id){
+  let text = document.getElementById(`input-${id}`);
+  let precio=parseInt(text.textContent)
+  let event=document.querySelector(`.input-number${id}`)
+  let input
+  event.addEventListener('change',(e)=>{
+    console.log(e.target.value)
+    let value=parseInt(e.target.value)
+     text.textContent=`${(precio*parseInt(e.target.value))/(value-1)}`
+  })
+}
 function handleclick(id){
-    console.log('first')
     let esta=carrito.some(element=>element._id===id);
-    console.log(esta)
     if(esta){
         carrito=carrito.filter(element=>element._id!==id);
     }
@@ -43,3 +54,13 @@ function handleclick(id){
     renderCards(carrito,contenedor)
 };
 
+btnClear.addEventListener('click',(e)=>{
+  localStorage.clear()
+  carrito=[]
+  renderCards(carrito,contenedor)
+  e.preventDefault()
+      Swal.fire({
+          title: "Compra Finalizada",
+          icon: "success",
+      });
+})
