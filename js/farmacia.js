7a7c51bc423a4b0338613afc876961c40e2d472e
Lazaro Tomas Del Prado <lazaro.delprado@alu.frt.utn.edu.ar>
2022-12-22 22:26:43 -0300

    se agrega filtros de busqueda

--- js/farmacia.js
+++ js/farmacia.js
@@ -1,30 +1,31 @@
+const searchContainer = document.getElementById("searchContainer")
 let contenedorCartas = document.getElementById("contenedorFarmacia")
-let carrito=JSON.parse(localStorage.getItem('carrito')) || []
+let carrito = JSON.parse(localStorage.getItem('carrito')) || []
 let farmacia;
 
 fetch("https://mindhub-xj03.onrender.com/api/petshop")
-    .then( response => response.json())
-    .then((data)=> {
+    .then(response => response.json())
+    .then((data) => {
         farmacia = data.filter((element) => element.categoria === "farmacia")
-        renderCards (farmacia,contenedorCartas)
+        renderCards(farmacia, contenedorCartas)
     })
     .catch(error => console.log(error))
-function renderCards (datos,contenedor){
-    contenedor.innerHTML=''
-    let farmacia=''
-    datos.forEach(element => { 
-        let btnId=`btn-${element._id}`
-        let esta=carrito.some(e=>e._id===element._id)
-        console.log(esta)
+
+function renderCards(datos, contenedor) {
+    contenedor.innerHTML = ''
+    let farmacia = ''
+    datos.forEach(element => {
+        let btnId = `btn-${element._id}`
+        let esta = carrito.some(e => e._id === element._id)
         let textBtn
-        if(esta){
-            textBtn='Eliminar Del Carrito'
-        }else{
-            textBtn='Agregar al Carrito'
+        if (esta) {
+            textBtn = 'Agregar al Carrito'
+        } else {
+            textBtn = 'Agregar al Carrito'
         }
-        if(element.disponibles!==0){
-            farmacia += 
-            `<div class="card1">
+        if (element.disponibles !== 0) {
+            farmacia +=
+                `<div class="card1">
                 <div class="card-img"><img src="${element.imagen}" class="card-img-top" alt="..."></div>
                 <div class="card-info">
                 <p class="text-title"> ${element.producto} </p>
@@ -37,8 +38,8 @@ function renderCards (datos,contenedor){
             </div>
         </div>`
         } else {
-            farmacia += 
-            `<div class="card1">
+            farmacia +=
+                `<div class="card1">
                 <div class="card-img"><img src="${element.imagen}" class="card-img-top" alt="..."></div>
                 <div class="card-info">
                 <p class="text-title"> ${element.producto} </p>
@@ -49,21 +50,37 @@ function renderCards (datos,contenedor){
                 </div>
             </div>`
         }
-        
-});
-    contenedor.innerHTML=farmacia 
+
+    });
+    contenedor.innerHTML = farmacia
 }
-function handleclick(id){
-    let btn=document.getElementById(`btn-${id}`);
-    let esta=carrito.some(element=>element._id===id);
-    if(esta){
-        carrito=carrito.filter(element=>element._id!==id)
-        btn.textContent='Agregar al Carrito';
-    }else{
-        let card=farmacia.find(element=>element._id===id)
+
+function handleclick(id) {
+    let btn = document.getElementById(`btn-${id}`);
+    let esta = carrito.some(element => element._id === id);
+    if (esta) {
+        carrito = carrito.filter(element => element._id !== id)
+        btn.textContent = 'Agregar al Carrito';
+    } else {
+        let card = farmacia.find(element => element._id === id)
         carrito.push(card);
-        btn.textContent='Eliminar Del Carrito';
+        btn.textContent = 'Eliminar Del Carrito';
     }
-    localStorage.setItem('carrito',JSON.stringify(carrito))
+    localStorage.setItem('carrito', JSON.stringify(carrito))
     console.log(carrito)
 };
+
+const search = document.getElementById("search");
+
+function filtrarPorBusqueda(datos, buscardor) {
+    return datos.filter(dato => dato.producto.toLowerCase().includes(buscardor.toLowerCase()))
+}
+
+searchContainer.addEventListener("input", () => {
+    let filtrarBusqueda = filtrarPorBusqueda(farmacia, search.value)
+    if (filtrarBusqueda.length <= 0) {
+        contenedorCartas.innerHTML = `<h2 class="h2Busqueda">NO SE ENCONTRARON RESULTADOS</h2>`;
+    } else {
+        renderCards(filtrarBusqueda, contenedorCartas);
+    }
+});
